cceb92b2ca49824235f7a579d2b0c4cd5bdd6dbe
cEbuka <kastecebuka@gmail.com>
2025-05-18 12:34:53 +0100

    added numbers and operator keys fuctionalities

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// work on functionalities
+const numbers = document.querySelectorAll(".calc__key--number");
+const clear = document.querySelector(".calc__key--clear");
+const backspace = document.querySelector(".calc__key--delete");
+const prefix = document.querySelector(".calc__key--prefix");
+const decimal = document.querySelector(".calc__key--decimal");
+const operators = document.querySelectorAll(".calc__key--operator");
+const display = document.querySelector(".calc__display");
+const equal = document.querySelector(".calc__key--equal");
+let result = 0;
+
+function displayContent() {
+    // show numbers on display
+    numbers.forEach((key) => {
+        key.addEventListener("click", () => {
+            display.textContent += key.innerHTML;
+            console.log(key);
+        })
+    })
+    //clear key
+    clear.addEventListener("click", () => {
+        clearDisplay();
+    })
+    // delete key
+    backspace.addEventListener("click", () => {
+        deleteContent();
+    })
+    // show operator keys
+    operators.forEach((key) => {
+        key.addEventListener("click", () => {
+            // get last character
+            const last = display.textContent.slice(-1);
+            // if display is not  clear and last character is not an operator
+            if (display.textContent !== "" && key.textContent !== last ) {
+                display.textContent += key.innerHTML;
+            }
+
+        })
+    })
+}
+
+function clearDisplay() {
+    display.textContent = "";
+}
+
+function deleteContent() {
+    display.textContent = display.textContent.slice(0, -1);
+
+}
+
+displayContent();
