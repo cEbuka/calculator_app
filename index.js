bb290a1d203cc5fb0982af2bba16db4a519c9bc7
cEbuka <kastecebuka@gmail.com>
2025-05-25 12:40:36 +0100

    fixed minus prefix and decimal button functions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const backspace = document.querySelector(".calc__key--delete");
 const prefix = document.querySelector(".calc__key--prefix");
 const decimal = document.querySelector(".calc__key--decimal");
 const operators = document.querySelectorAll(".calc__key--operator");
-const display = document.querySelector(".calc__display");
+let display = document.querySelector(".calc__display");
 const equal = document.querySelector(".calc__key--equal");
 let result = 0;
 
@@ -13,7 +13,8 @@ function displayContent() {
     // show numbers on display
     numbers.forEach((key) => {
         key.addEventListener("click", () => {
-            display.textContent += key.innerHTML;
+            display.value += key.textContent;
+            console.log(display);
         })
     })
     //clear key
@@ -28,44 +29,62 @@ function displayContent() {
     operators.forEach((key) => {
         key.addEventListener("click", () => {
             // get last character
-            const last = display.textContent.slice(-1);
+            const last = display.value.slice(-1);
             // if display is not clear and last character is not an operator
-            if (display.textContent !== "" && key.textContent !== last) {
-                display.textContent += key.innerHTML;
+            if (display.value !== "" && key.textContent !== last) {
+                display.value += key.innerHTML;
             }
 
 
         })
     })
 
-    // prefix key
-    // todo: fix multiple instances
-    prefix.addEventListener("click", () => {
 
-        if (display.textContent !== " " && display.textContent.slice(0) !== "-") {
-            display.textContent = "-" + display.textContent
+    prefix.addEventListener("click", () => {
+        const value = display.value;
+
+        if (value.includes("-")) return;
+        else {
+            display.value = "-" + display.value;
+            return;
         }
+
+
     })
 
     // decimal key
-    // todo: fix decimal point
     decimal.addEventListener("click", () => {
+        const value = display.value;
 
-        if (display.textContent.slice(-1) !== decimal.textContent && !(display.textContent.includes("."))) {
-            display.textContent += decimal.textContent;
-            console.log(number)
+        // if display is empty and is . pressed, show 0.
+        if (value === "") {
+            display.value += "0.";
+            return;
         }
+        // split the text into parts based on the operators
+        const parts = value.split(/[\+\-\*\/\%]/);
+        // the last part in the splits
+        const currentNumber = parts[parts.length - 1];
+        //    if the last number is empty, insert 0. if . is clicked
+        if (currentNumber === '') {
+            display.value += "0.";
+            return;
+        }
+        // if the last part contains a ., do nothing
+        if (currentNumber.includes(".")) return;
+        // show the decimal point
+        display.value += decimal.textContent;
     })
 
 
 }
 
 function clearDisplay() {
-    display.textContent = "";
+    display.value = "";
 }
 
 function deleteContent() {
-    display.textContent = display.textContent.slice(0, -1);
+    display.value = display.value.slice(0, -1);
 
 }
 
